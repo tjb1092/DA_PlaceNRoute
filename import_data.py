5f89c18c64665dd28c66621a1ab7a392520d7c35
Tony Bailey <baileytj@mail.uc.edu>
2019-03-21 17:00:35 -0400

    Copied over files from first assignment. Redid the importing scheme.

---
+++ import_data.py
@@ -0,0 +1,150 @@
+import random
+
+class Cell:
+	def __init__(self, num):
+		self.num = num
+		self.nbrs = {}  # constant time lookup for membership/weight
+
+class Net:
+	def __init__(self, num):
+		self.num = num
+		self.terminals = {}
+
+
+class ConnectivityList:
+	def __init__(self, num_cells, num_nets):
+		self.num_cells = num_cells
+		self.num_nets = num_nets
+
+		self.cells = {}  # constant time lookup for membership/weight
+		for i in range(1, self.num_cells+1):
+			# Instantiate all cells
+			self.cells[i] = Cell(i)
+
+		print(self.cells[45].nbrs)
+
+		self.nets = {}
+		for i in range(1, self.num_nets+1):
+			# Instantiate all nets
+			self.nets[i] = Net(i)
+
+
+
+	def add_net(self, net):
+		# net is a list of length 5
+		# [0-net#, 1-cell1#, 2-term1#, 3-cell2#, 2-term2#]
+
+		# add 0->1 net
+		print(net)
+		print(self.cells[net[1]].nbrs)
+		if net[3] in self.cells[net[1]].nbrs:
+			# if it's a repeat, add 1 to "weight"
+			self.cells[net[1]].nbrs[net[3]] += 1
+		else:
+			#else, create connection
+			self.cells[net[1]].nbrs[net[3]] = 1
+
+		print(self.cells[net[1]].nbrs)
+		# add 1->0 net
+		if net[1] in self.cells[net[3]].nbrs:
+			# if it's a repeat, add 1 to "weight"
+			self.cells[net[3]].nbrs[net[1]] += 1
+		else:
+			#else, create connection
+			self.cells[net[3]].nbrs[net[1]] = 1
+		self.nets[net[0]].terminals = {net[1]:net[2], net[3]: net[4]}
+
+
+class GroupLst:
+	def __init__(self, graph, num_cells):
+		self.graph = graph  # pointer to adjacency list
+		self.num_cells = num_cells
+		# initialize group array to have 50% group A and 50% group B.
+		self.V = [0]*(self.num_cells//2) + [1]*(self.num_cells//2)
+		random.shuffle(self.V)  # initialize random partition
+		self.init_cost()  # Initialize the cost of the random partition
+
+	def init_cost(self):
+		cost = 0
+		for i, n in enumerate(self.V):
+			# Find 0 cells w/ connections in 1
+			if n == 0:
+				if i+1 in self.graph.cells:
+					for nbr in self.graph.cells[i+1].nbrs:
+						if self.V[nbr-1] == 1:
+							# Add their weights
+							cost += self.graph.cells[i+1].nbrs[nbr]
+		self.cost = cost
+
+	def perturb(self):
+		# randomly switch two cells
+		A = random.randint(0,self.num_cells-1)
+		chosen = False
+		while not chosen:
+			# Ensures that the B cell is not in A
+			B = random.randint(0,self.num_cells-1)
+			if B == A or self.V[B] == self.V[A]:
+				continue
+			else:
+				chosen = True
+		# Switch the groups
+		self.V[A], self.V[B] = int(not(self.V[A])), int(not(self.V[B]))
+
+		# Update the costs based on that move
+		self.update_cost(A+1, B+1)  # The +1 offsets the 0-index of the list
+		self.update_cost(B+1, A+1)
+
+	def update_cost(self, A, B):
+		# get A's group
+		gA = self.V[A-1]
+		if A in self.graph.cells:
+			# Traverse through all of A's nbrs.
+			for nbr in self.graph.cells[A].nbrs:
+				if self.V[nbr-1] == gA and nbr != (B):
+					# Reduce cost if nbr is now in the same group as A
+					self.cost = self.cost - self.graph.cells[A].nbrs[nbr]
+				elif self.V[nbr-1] != gA and nbr != (B):
+					# Increase "    "
+					self.cost = self.cost + self.graph.cells[A].nbrs[nbr]
+
+def data_load(fn):
+	# Open file and read its contents
+	with open(fn) as f:
+		content = f.readlines()
+		f.close()
+	# Extract the number of cells and nets
+	num_cells, num_nets = int(content[0][:-1]), int(content[1][:-1])
+	connect_lst = ConnectivityList(num_cells, num_nets)
+
+	print(num_cells, num_nets)
+
+	for row in content[2:]:
+		# For each row, add the net to the graph
+		cells = row[:-1].split(" ")
+		print(cells)
+		connect_lst.add_net([int(x) for x in cells])
+
+	#print([(x.num,x.terminals) for x in connect_lst.nets.values()])
+	#print([(x.num,x.nbrs) for x in connect_lst.cells.values()])
+	return connect_lst
+
+
+def writeResults(solution, cost, fn):
+	# Write the cost and cutsets to an output file.
+	f = open("Results/{}".format(fn), "w")
+	newline = str(cost)+ "\n"  # Generate cost string
+	f.write(newline)
+	A, B = "", ""
+	# Generate strings for the A group and the B group
+	for i, n in enumerate(solution):
+		if n == 0:
+			A += (str(i+1) + " ")
+		else:
+			B += (str(i+1) + " ")
+
+	f.write(A+"\n")
+	f.write(B+"\n")
+	f.close()
+
+if __name__ == "__main__":
+	data_load("Example-Netlists/1")
---
+++ parprog.py
@@ -0,0 +1,165 @@
+from data_handler  import data_load, GroupLst, writeResults
+import math
+import random
+import time
+import os
+import matplotlib.pyplot as plt
+import argparse
+from pympler import classtracker
+
+def acceptMove(delCost, T, num_cells):
+    #Logic to accept a move
+    if delCost < 0:
+        #If cost improves, accept
+        return 0, True
+    else:
+        #Compute the boltz constant, scaled by the number of cells.
+        boltz = math.exp((-(delCost*num_cells))/(T))
+        r = random.uniform(0, 1)  # Pick a random number between 0 and 1.
+        #Probabilstically determine if the move should be accepted.
+        if r < boltz:
+            return boltz, True
+        else:
+            return boltz, False
+
+def coolDown(T, params):
+    # Reduce T by a factor of Trate
+    return T * params["Trate"]
+
+
+def simulatedAnnealing(graph, num_cells, params, fn):
+    cltr = classtracker.ClassTracker()
+    cltr.track_class(GroupLst)  # Follow the memory size of the graph data struct.
+
+    # Instantiate GroupLst objects
+    curSol, nextSol = GroupLst(graph, num_cells), GroupLst(graph, num_cells)
+    bestSol, bestCost = list(curSol.V), curSol.cost
+
+    T = params["T0"]  # Initial Temp
+
+    # Initialize graphing variables
+    last_time, total_time = time.time(), time.time()
+    iter_cntr,tmp_cntr = 0, 0
+    iter_arr, temp_arr = [], []
+    best_cutset_arr, cutset_arr, t2_arr, boltz_arr, nam_arr = [], [], [], [], []
+    print_skip = 10
+
+    while T > params["Tfreeze"]:
+        #While the temperature is above Tfreeze,
+
+        num_accepted_moves = 0  # reset accepted moves counter
+
+        for i in range(params["num_moves_per_T"]):
+            # For the number of moves per T,
+            # copy over the curSol into the nextSol.
+            nextSol.V, nextSol.cost = list(curSol.V), curSol.cost
+            nextSol.perturb()  # Swap two random cells and update the cost.
+            delCost = nextSol.cost - curSol.cost
+            boltz, accept = acceptMove(delCost, T, num_cells)
+            if accept:
+                #If the move is accepted, copy nextSol back into curSol
+                curSol.V, curSol.cost = list(nextSol.V), nextSol.cost
+                num_accepted_moves += 1
+
+            if curSol.cost < bestCost:
+                # If curSol is the best solution seen so far, upate bestSol.
+                bestSol, bestCost = list(curSol.V), curSol.cost
+
+
+            iter_cntr += 1
+
+        # Update graph data. Only once per temp to save space
+        iter_arr.append(iter_cntr)
+        t2_arr.append(T)
+        temp_arr.append(T)
+        cutset_arr.append(curSol.cost)
+        best_cutset_arr.append(bestCost)
+        nam_arr.append(num_accepted_moves)
+        boltz_arr.append(boltz)
+        tmp_cntr += 1
+
+        if tmp_cntr % print_skip == 0:
+            # Give output every print_skip temp updates
+            print("{} T: {} curCost: {}, num_accepted_moves: {}".format(tmp_cntr, T, curSol.cost, num_accepted_moves))
+            print('T step took {:0.3f} seconds'.format((time.time()-last_time)/print_skip))
+            last_time = time.time()
+
+        T = coolDown(T, params)
+
+    # Plot the requisite graphs
+    fig, ax = plt.subplots()
+    ax.plot(temp_arr, cutset_arr, label="Cutset Size")
+    ax.plot(temp_arr, best_cutset_arr, label="Best Cutset Size")
+    ax.set_xlabel("Temperature")
+    ax.set_ylabel("Cutset Size")
+    ax.set_title("Cutset Size vs. Temperature")
+    ax.legend()
+    fig.set_size_inches(6, 4)
+    fig.savefig("Images/{}_CS_v_T.png".format(fn), dpi=200)
+
+    fig, ax = plt.subplots()
+    ax.plot(iter_arr, t2_arr)
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Temperature")
+    ax.set_title("Temperature vs. Iteration")
+    fig.set_size_inches(6, 4)
+    fig.savefig("Images/{}_T_v_I.png".format(fn), dpi=200)
+
+    fig, ax = plt.subplots()
+    ax.plot(iter_arr, boltz_arr)
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Boltz")
+    ax.set_title("Boltz vs. Iteration")
+    fig.set_size_inches(6, 4)
+    fig.savefig("Images/{}_B_v_I.png".format(fn), dpi=200)
+
+    fig, ax = plt.subplots()
+    ax.plot(temp_arr, nam_arr)
+    ax.set_xlabel("Temperature")
+    ax.set_ylabel("Number of Accepted Moves")
+    ax.set_title("Number of Accepted Moves vs. Temperature")
+    fig.set_size_inches(6, 4)
+    fig.savefig("Images/{}_NAM_v_T.png".format(fn), dpi=200)
+
+    print("\n\nBest Cost: {}, temp iteration {}".format(bestCost, tmp_cntr))
+    print("Total Time {:0.3f} seconds".format(time.time()-total_time))
+
+    cltr.create_snapshot()
+    cltr.stats.print_summary()  #display the size of the instatiated GroupLst data structs
+
+    return bestSol, bestCost
+
+
+def main():
+    # Needed for the specified command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', type=str, default="1",
+                        help='What is the input filename?')
+    parser.add_argument('-o', type=str, default="R_1",
+                        help='What is the output filename?')
+    args = parser.parse_args()
+
+    # generate the output file directories if the don't exist
+    if not os.path.exists("Results"):
+        os.makedirs("Results")
+    if not os.path.exists("Images"):
+        os.makedirs("Images")
+
+    # Load the input data and generate the graph
+    connect_lst = data_load(args.i)
+    print("Graph Created")
+
+    #nmpt = int(0.0542*(num_cells*num_nets)**0.4921+10)  # Heuristic to scale number of iterations
+
+    #params = {"T0": 10000, "Tfreeze": 0.01, "num_moves_per_T": nmpt, "Trate":0.99}  # k=1, so it won't be considered
+
+    # Execute placement engine.
+    solution, cost = placement(connect_lst)
+
+    print("num moves per T {}".format(nmpt))
+    # Write the solution to output file
+    writeResults(solution, cost, args.o)
+
+
+if __name__ == "__main__":
+    main()
