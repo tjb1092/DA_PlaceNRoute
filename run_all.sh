7c8f757e6f73a84dd52a68f514e4dfa299a2935a
Tony Bailey <baileytj@mail.uc.edu>
2019-04-12 08:17:43 -0400

    Add a script to run all cases.

---
+++ run_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Runs the python scripts to generate data for report
+
+files='1 2 3 4 5 6'
+
+for file in $files
+do
+	#
+	/usr/bin/time -v python3 parprog.py -i Results/$file -o Results/$file > Performance/$file
+done
+
+
+files='b_50_50 b_100_100 b_400_400 b_600_1000 b_900_800 b_1000_1200 b_1200_1500 b_1500_1500 b_2000_2000'
+
+for file in $files
+do
+	#
+	/usr/bin/time -v python3 parprog.py -i Benchmarks2/$file -o Results/$file > Performance/$file
+done
